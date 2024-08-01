8d5af8645d0b923ea42bd654f83a4e2b330c8047
mrenglerdavid <150632297+mrenglerdavid@users.noreply.github.com>
2024-08-01 19:57:25 +0200

    textSplitter.js

--- textSplitter.js
+++ textSplitter.js
@@ -3,18 +3,7 @@ import { debounce } from './common.js';
 
 // Defines a class to split text into lines, words and characters for animation.
 export class TextSplitter {
-  // Constructor for TextScrollEffect which sets up the text animation.
-  // Parameters:
-  //   textElement: HTMLElement - The DOM element that contains the text to be animated.
-  //   options: Object (optional) - Configuration options for the text splitting and callbacks.
-  //     options.resizeCallback: Function - A function to call on window resize events.
-  //     options.splitTypeTypes: String - Specifies the types of splits to perform on the text. 
-  //         Possible values are based on SplitType's configuration, such as 'lines', 'words', 'chars'.
-  //         See SplitType documentation for more details: https://github.com/lukePeavey/SplitType
-  // This constructor initializes the text splitting with specified options, sets up resize handling,
-  // and prepares the text element for animation effects.
   constructor(textElement, options = {}) {
-    // Ensure the textElement is a valid HTMLElement.
     if (!textElement || !(textElement instanceof HTMLElement)) {
       throw new Error('Invalid text element provided.');
     }
@@ -22,54 +11,54 @@ export class TextSplitter {
     const { resizeCallback, splitTypeTypes } = options;
     
     this.textElement = textElement;
-    // Assign the resize callback if provided and is a function, otherwise null.
     this.onResize = typeof resizeCallback === 'function' ? resizeCallback : null;
-    
-    // Set options for SplitType based on provided splitTypeTypes or default to SplitType's default behavior.
-    // The 'types' option allows customization of how text is split (e.g., into lines, words, characters).
-    // Refer to SplitType documentation for possible values and updates: https://github.com/lukePeavey/SplitType
     const splitOptions = splitTypeTypes ? { types: splitTypeTypes } : {};
     this.splitText = new SplitType(this.textElement, splitOptions);
 
-    // Initialize ResizeObserver to re-split text on resize events, if a resize callback is provided.
     if (this.onResize) {
-      this.initResizeObserver(); // Set up observer to detect resize events.
+      this.initResizeObserver();
     }
+
+    // Ensure words are not cut off by setting minimum height for each word.
+    this.adjustHeight();
   }
 
-  // Sets up ResizeObserver to re-split text on element resize.
   initResizeObserver() {
-    this.previousContainerWidth = null; // Track element width to detect resize.
-
+    this.previousContainerWidth = null;
     let resizeObserver = new ResizeObserver(
       debounce((entries) => this.handleResize(entries), 100)
     );
-    resizeObserver.observe(this.textElement); // Start observing the text element.
+    resizeObserver.observe(this.textElement);
   }
 
-  // Handles element resize, re-splitting text if width changes.
   handleResize(entries) {
     const [{ contentRect }] = entries;
     const width = Math.floor(contentRect.width);
-    // If element width changed, re-split text and call resize callback.
-    if ( this.previousContainerWidth && this.previousContainerWidth !== width ) {
-      this.splitText.split(); // Re-split text for new width.
-      this.onResize(); // Execute the callback function.
+    if (this.previousContainerWidth && this.previousContainerWidth !== width) {
+      this.splitText.split();
+      this.onResize();
     }
-    this.previousContainerWidth = width; // Update stored width.
+    this.previousContainerWidth = width;
   }
 
-  // Returns the lines created by splitting the text element.
+  adjustHeight() {
+    const words = this.getWords();
+    words.forEach(word => {
+      const height = word.offsetHeight;
+      if (height < 40) {
+        word.style.height = '40px'; // Ensuring the height is sufficient
+      }
+    });
+  }
+
   getLines() {
     return this.splitText.lines;
   }
 
-  // Returns the words created by splitting the text element.
   getWords() {
     return this.splitText.words;
   }
 
-  // Returns the chars created by splitting the text element.
   getChars() {
     return this.splitText.chars;
   }
