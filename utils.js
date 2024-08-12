8c9cf32763531df36ff7df8182196a43c712fb4a
mrenglersite <150632297+mrenglerdavid@users.noreply.github.com>
2024-08-12 18:52:08 +0200

    utils.js

--- utils.js
+++ utils.js
@@ -6,11 +6,9 @@ export const preloadFonts = (id) => {
         urls: ['https://cdn.prod.website-files.com/66a8b5ee49d126b699969fd3/css/elana-braut--abendmoden.webflow.816e607d2.css']
       },
       active: () => {
-        console.log("WebFont active"); // Log to verify WebFont is active
         resolve();
       },
       inactive: () => {
-        console.error("WebFont inactive"); // Log if WebFont failed to load
         resolve();
       }
     });
