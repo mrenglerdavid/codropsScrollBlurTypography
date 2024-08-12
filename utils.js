8db8305094ff28152c20e6e50cc95e4facd294da
mrenglersite <150632297+mrenglerdavid@users.noreply.github.com>
2024-08-12 16:57:44 +0200

    utils.js

--- utils.js
+++ utils.js
@@ -2,13 +2,14 @@
  * Preload fonts
  * @param {String} id
  */
-export const preloadFonts = id => {
+export const preloadFonts = () => {
   return new Promise((resolve) => {
-      WebFont.load({
-          typekit: {
-              id: id
-          },
-          active: resolve
-      });
+    WebFont.load({
+      custom: {
+        families: ['Cera Pro:700,100'],
+        urls: ['https://cdn.prod.website-files.com/66a8b5ee49d126b699969fd3/css/elana-braut--abendmoden.webflow.816e607d2.css']
+      },
+      active: resolve
+    });
   });
 };
