713cd5eab1ab540f333102e9378b43a6268dc236
mrenglersite <150632297+mrenglerdavid@users.noreply.github.com>
2024-08-12 17:51:07 +0200

    utils.js

--- utils.js
+++ utils.js
@@ -1,15 +1,18 @@
-/**
- * Preload fonts
- * @param {String} id
- */
-export const preloadFonts = () => {
+export const preloadFonts = (id) => {
   return new Promise((resolve) => {
     WebFont.load({
       custom: {
         families: ['Cera Pro:700,100'],
         urls: ['https://cdn.prod.website-files.com/66a8b5ee49d126b699969fd3/css/elana-braut--abendmoden.webflow.816e607d2.css']
       },
-      active: resolve
+      active: () => {
+        console.log("WebFont active"); // Log to verify WebFont is active
+        resolve();
+      },
+      inactive: () => {
+        console.error("WebFont inactive"); // Log if WebFont failed to load
+        resolve();
+      }
     });
   });
 };
