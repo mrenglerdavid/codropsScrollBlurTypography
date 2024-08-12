2ab26b9c9aa23ffa3cd392af7255fdd7b8fc514c
mrenglersite <150632297+mrenglerdavid@users.noreply.github.com>
2024-08-12 18:50:33 +0200

    index.js

--- index.js
+++ index.js
@@ -4,21 +4,18 @@ import { preloadFonts } from './utils.js';
 gsap.registerPlugin(ScrollTrigger);
 
 const init = () => {
-  console.log("Init function called"); // Log to verify init is called
   const effects = [
     { selector: '[data-effect-1]', effect: BlurScrollEffect1 }
   ];
 
   effects.forEach(({ selector, effect }) => {
     document.querySelectorAll(selector).forEach(el => {
-      console.log("Applying effect to element:", el); // Log each element the effect is applied to
       new effect(el);
     });
   });
 };
 
 preloadFonts('lnu1fpi').then(() => {
-  console.log("Fonts loaded"); // Log to verify fonts are loaded
   document.body.classList.remove('loading');
   init();
 });
