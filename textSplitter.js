5a5096978ded3f3d45aa0ce6f301d74008c53ca8
mrenglersite <150632297+mrenglerdavid@users.noreply.github.com>
2024-08-20 12:14:09 +0200

    textSplitter.js

--- textSplitter.js
+++ textSplitter.js
@@ -1,52 +1,76 @@
-// Import the TextSplitter class for handling text splitting.
-import { TextSplitter } from './textSplitter.js';
+// Import debounce utility function.
+import { debounce } from './common.js';
 
-// Defines a class to create scroll-triggered animation effects on text.
-export class BlurScrollEffect {
-  constructor(textElement) {
-    // Check if the provided element is valid.
+// Defines a class to split text into lines, words and characters for animation.
+export class TextSplitter {
+  // Constructor for TextScrollEffect which sets up the text animation.
+  // Parameters:
+  //   textElement: HTMLElement - The DOM element that contains the text to be animated.
+  //   options: Object (optional) - Configuration options for the text splitting and callbacks.
+  //     options.resizeCallback: Function - A function to call on window resize events.
+  //     options.splitTypeTypes: String - Specifies the types of splits to perform on the text. 
+  //         Possible values are based on SplitType's configuration, such as 'lines', 'words', 'chars'.
+  //         See SplitType documentation for more details: https://github.com/lukePeavey/SplitType
+  // This constructor initializes the text splitting with specified options, sets up resize handling,
+  // and prepares the text element for animation effects.
+  constructor(textElement, options = {}) {
+    // Ensure the textElement is a valid HTMLElement.
     if (!textElement || !(textElement instanceof HTMLElement)) {
       throw new Error('Invalid text element provided.');
     }
 
-    this.textElement = textElement;
-
-    // Set up the effect for the provided text element.
-    this.initializeEffect();
-  }
-
-  // Sets up the initial text effect on the provided element.
-  initializeEffect() {
-    // Callback to re-trigger animations on resize.
-    const textResizeCallback = () => this.scroll();
-
-    // Split text for animation and store the reference.
-    this.splitter = new TextSplitter(this.textElement, {
-      resizeCallback: textResizeCallback,
-      splitTypeTypes: 'words, chars'
-    });
+    const { resizeCallback, splitTypeTypes } = options;
     
-    // Trigger the initial scroll effect.
-    this.scroll();
+    this.textElement = textElement;
+    // Assign the resize callback if provided and is a function, otherwise null.
+    this.onResize = typeof resizeCallback === 'function' ? resizeCallback : null;
+    
+    // Set options for SplitType based on provided splitTypeTypes or default to SplitType's default behavior.
+    // The 'types' option allows customization of how text is split (e.g., into lines, words, characters).
+    // Refer to SplitType documentation for possible values and updates: https://github.com/lukePeavey/SplitType
+    const splitOptions = splitTypeTypes ? { types: splitTypeTypes } : {};
+    this.splitText = new SplitType(this.textElement, splitOptions);
+
+    // Initialize ResizeObserver to re-split text on resize events, if a resize callback is provided.
+    if (this.onResize) {
+      this.initResizeObserver(); // Set up observer to detect resize events.
+    }
   }
 
-  // Animates text based on the scroll position.
-  scroll() {
-    // Query all individual characters in the line for animation.
-    const chars = this.splitter.getChars();
-    gsap.fromTo(chars, {
-      filter: 'blur(10px) brightness(70%)',
-      willChange: 'filter'
-    }, {
-        ease: 'none', // Animation easing.
-        filter: 'blur(0px) brightness(100%)',
-        stagger: 0.02, // Delay between starting animations for each character.
-        scrollTrigger: {
-          trigger: this.textElement, // Element that triggers the animation.
-          start: 'top bottom-=15%', // Animation starts when element hits bottom of viewport.
-          end: 'bottom center+=15%', // Animation ends in the center of the viewport.
-          scrub: true, // Animation progress tied to scroll position.
-        },
-    });
+  // Sets up ResizeObserver to re-split text on element resize.
+  initResizeObserver() {
+    this.previousContainerWidth = null; // Track element width to detect resize.
+
+    let resizeObserver = new ResizeObserver(
+      debounce((entries) => this.handleResize(entries), 100)
+    );
+    resizeObserver.observe(this.textElement); // Start observing the text element.
+  }
+
+  // Handles element resize, re-splitting text if width changes.
+  handleResize(entries) {
+    const [{ contentRect }] = entries;
+    const width = Math.floor(contentRect.width);
+    // If element width changed, re-split text and call resize callback.
+    if ( this.previousContainerWidth && this.previousContainerWidth !== width ) {
+      this.splitText.split(); // Re-split text for new width.
+      this.onResize(); // Execute the callback function.
+    }
+    this.previousContainerWidth = width; // Update stored width.
+  }
+
+  // Returns the lines created by splitting the text element.
+  getLines() {
+    return this.splitText.lines;
+  }
+
+  // Returns the words created by splitting the text element.
+  getWords() {
+    return this.splitText.words;
+  }
+
+  // Returns the chars created by splitting the text element.
+  getChars() {
+    return this.splitText.chars;
   }
 }
