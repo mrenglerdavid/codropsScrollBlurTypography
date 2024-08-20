e9dcf6f66007ca10bbf1c0e0a8d10f30a9c2f094
mrenglersite <150632297+mrenglerdavid@users.noreply.github.com>
2024-08-20 12:04:10 +0200

    textSplitter.js

--- textSplitter.js
+++ textSplitter.js
@@ -1,65 +1,40 @@
-// Import debounce utility function.
-import { debounce } from './common.js';
+import { TextSplitter } from '../textSplitter.js';
 
-// Defines a class to split text into lines, words and characters for animation.
-export class TextSplitter {
-  constructor(textElement, options = {}) {
+export class BlurScrollEffect {
+  constructor(textElement) {
     if (!textElement || !(textElement instanceof HTMLElement)) {
       throw new Error('Invalid text element provided.');
     }
-
-    const { resizeCallback, splitTypeTypes } = options;
-    
     this.textElement = textElement;
-    this.onResize = typeof resizeCallback === 'function' ? resizeCallback : null;
-    const splitOptions = splitTypeTypes ? { types: splitTypeTypes } : {};
-    this.splitText = new SplitType(this.textElement, splitOptions);
-
-    if (this.onResize) {
-      this.initResizeObserver();
-    }
-
-    // Ensure words are not cut off by setting minimum height for each word.
-    this.adjustHeight();
+    this.initializeEffect();
   }
 
-  initResizeObserver() {
-    this.previousContainerWidth = null;
-    let resizeObserver = new ResizeObserver(
-      debounce((entries) => this.handleResize(entries), 100)
-    );
-    resizeObserver.observe(this.textElement);
-  }
-
-  handleResize(entries) {
-    const [{ contentRect }] = entries;
-    const width = Math.floor(contentRect.width);
-    if (this.previousContainerWidth && this.previousContainerWidth !== width) {
-      this.splitText.split();
-      this.onResize();
-    }
-    this.previousContainerWidth = width;
-  }
-
-  adjustHeight() {
-    const words = this.getWords();
-    words.forEach(word => {
-      const height = word.offsetHeight;
-      if (height < 40) {
-        word.style.height = '40px'; // Ensuring the height is sufficient
-      }
+  initializeEffect() {
+    const textResizeCallback = () => this.scroll();
+    this.splitter = new TextSplitter(this.textElement, {
+      resizeCallback: textResizeCallback,
+      splitTypeTypes: 'words, chars'
     });
+    this.scroll();
   }
 
-  getLines() {
-    return this.splitText.lines;
-  }
-
-  getWords() {
-    return this.splitText.words;
-  }
-
-  getChars() {
-    return this.splitText.chars;
+  scroll() {
+    const chars = this.splitter.getChars();
+    gsap.fromTo(chars, {
+      opacity: 0,
+      y: 10,
+      willChange: 'opacity, transform'
+    }, {
+        opacity: 1,
+        y: 0,
+        ease: 'none',
+        stagger: 0.02,
+        scrollTrigger: {
+          trigger: this.textElement,
+          start: 'top bottom-=10%',
+          end: 'bottom center+=10%',
+          scrub: true,
+        },
+    });
   }
 }
